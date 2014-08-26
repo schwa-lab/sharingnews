338de5a026a4a6ac7c99a1327b2b10bb8d21bc49
Joel Nothman <joel.nothman@gmail.com>
2014-08-26 17:17:43 +1000

    improved downloading

--- scripts/download.py
+++ scripts/download.py
@@ -8,19 +8,24 @@ on each line.
 """
 from __future__ import print_function
 import requests
+import os
 import sys
 import datetime
 
 now = datetime.datetime.now
+PID = os.getpid()
 
 
 def get_mime(resp):
-    return resp.headers.get('content-type', '')
+    return resp.headers.get('content-type', '').split(';')[0]
 
 COMMENTABLE_MIMES = ('html', 'xhtml', 'xml')
 
 
-for l in sys.stdin:
+path = None
+for i, l in enumerate(sys.stdin):
+    if i % 100 == 0:
+        print(PID, now(), 'Downloaded {}. Latest to {}'.format(i + 1, path), file=sys.stderr)
     path, url = l.strip().split('\t')
     ts = now()
     with open(path + '.stat', 'w') as status_f:
@@ -38,7 +43,8 @@ for l in sys.stdin:
         content_f.write(response.content)
         if get_mime(response).split('/')[-1].lower() in COMMENTABLE_MIMES:
             print('\n'
-                  '<!-- requested at {} from {}'
+                  '<!-- requested at {} from {}\n'
                   '     response in {}s from {} -->'
                   ''.format(ts, url, response.elapsed, response.url),
                   file=content_f)
+print(PID, now(), 'Downloaded {}. Done.'.format(i), file=sys.stderr)
