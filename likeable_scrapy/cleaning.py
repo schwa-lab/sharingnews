21880c585e0d0bde89900d225f4b71372913516f
Joel Nothman <joel.nothman@gmail.com>
2014-09-01 11:08:23 +1000

    More normalisation of URL shape

--- likeable_scrapy/cleaning.py
+++ likeable_scrapy/cleaning.py
@@ -1,23 +1,30 @@
 import re
 import tldextract
+import urlparse
+import urllib
 
+
+DIGIT_RE = re.compile('[0-9]')
+# Standard use of params would require ;, but /a=b/c=d is also used
+PARAM_RE = re.compile('((?:;|^)[^=]+=)[^;/]*')
+SLUG_CHARS = r'[\w\'",$+()-]'
+SLUG_RE = re.compile(r'(?u){0}*[^\W\d]+{0}*'.format(SLUG_CHARS))
+INT_ID_RE = re.compile(r'\b0+([,_.]0+)+\b|\b0{8}[0-]+\b')
 
 def url_signature(url):
-    url = url.split('#')[0]
-    if '?' in url:
-        url, query = url.split('?', 1)
-        query = '&'.join(x.split('=')[0] for x in query.split('&'))
-    else:
-        query = ''
-    _, url = url.split('://', 1)
-    url = url.rstrip('/')
-    if '/' in url:
-        domain, path = url.split('/', 1)
-    else:
-        domain = url
-        path = ''
-    path = re.sub('[0-9]', '0', path)
-    path = re.sub('[0-9a-zA-Z_\'",$-]*[a-zA-Z]+[0-9a-zA-Z_\'",$-]*', 'a', path)
+    # TODO: doctests
+    _, domain, path, query, _ = urlparse.urlsplit(url)
+    query = '&'.join(urllib.unquote(x.split('=')[0]) for x in query.split('&'))
+    path = urllib.unquote(path)
+    path = DIGIT_RE.sub('0', path)
+    # XXX: unquoting might invent extra '/'s
+    path = '/'.join(urllib.unquote(PARAM_RE.sub(r'\1', part))
+                    for part in path.split('/'))
+    try:
+        path = SLUG_RE.sub('a', path.decode('utf8')).encode('utf8')
+    except UnicodeDecodeError:
+        path = SLUG_RE.sub('a', path)
+    path = INT_ID_RE.sub('ID', path)
     if domain.startswith('www.'):
         domain = domain[4:]
     return domain, path, query
