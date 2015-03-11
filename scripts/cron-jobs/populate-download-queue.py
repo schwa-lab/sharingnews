8455f7edd9f6c571c7c2e6063923a2045e1129ba
Joel Nothman <joel.nothman@gmail.com>
2015-03-11 12:58:42 +1100

    Fix already running case for cron jobs

--- scripts/cron-jobs/populate-download-queue.py
+++ scripts/cron-jobs/populate-download-queue.py
@@ -3,6 +3,7 @@ if [ "$(scripts/download-queue.py -H schwa10 count 2>/dev/null)" -eq 0 ]
 then
     if pgrep -f "scripts/ids-for-download.py" >/dev/null; then
         echo "ids-for-download process already running"
+        exit 1
     fi
     scripts/ids-for-download.py 5000 |
     scripts/download-queue.py -H schwa10 --log-path ~likeable/logs/fetch/fetch-schwa09-cron.log.bz2 enqueue 2>&1 | grep 'ERROR'
